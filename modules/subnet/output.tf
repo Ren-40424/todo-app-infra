a779a65a31f5fe19e5282fc054565a3997fd5349
Ren-40424 <takashimaren0527@gmail.com>
2025-06-17 04:08:12 +0000

    サブネットモジュールの出力方法を変更

--- modules/subnet/output.tf
+++ modules/subnet/output.tf
@@ -1,6 +1,13 @@
-output "ids_by_usage" {
-  value = {
-    for k, subnet in aws_subnet.this :
-    subnet.tags["Usage"] => subnet.id...
+locals {
+  subnet_ids_by_usage = {
+    for usage in distinct([for s in aws_subnet.this : s.tags["Usage"]]) :
+    usage => {
+      for k, s in aws_subnet.this :
+      var.subnets[k].az_suffix => s.id if s.tags["Usage"] == usage
+    }
   }
+}
+
+output "ids_by_usage" {
+  value = local.subnet_ids_by_usage
 }
